aec3449d0ee0c0ad2cea38ee89e7612287008b41
Jonas Krahn <joerg.jonas.krahn@iav.de>
2025-01-17 21:00:30 +0100

    chore: new concept

--- scripts/unix/generate-docs-packages.sh
+++ scripts/unix/generate-docs-packages.sh
@@ -4,8 +4,8 @@ echo "PACKAGE_NAME=$NAME" >> $GITHUB_ENV
 VERSION=$(node -p "require('./package.json').version")
 echo "PACKAGE_VERSION=$VERSION" >> $GITHUB_ENV
 
-echo "PACKAGE_DOCS_FOLDER=$NAME _ $VERSION" >> $GITHUB_ENV
+mkdir -p "../../generated_docs/${PACKAGE_NAME}/${PACKAGE_VERSION}"
 
-mkdir -p generated_docs/$PACKAGE_DOCS_FOLDER
+echo "../../generated_docs/${PACKAGE_NAME}/${PACKAGE_VERSION}"
 
-cp -r docs/* generated_docs/$PACKAGE_DOCS_FOLDER
+cp -r docs/* "../../generated_docs/${PACKAGE_NAME}/${PACKAGE_VERSION}"
