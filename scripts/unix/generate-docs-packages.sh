3733c495da174155e22adfd5fa1e27d73ab26004
Joerg Jonas Krahn <jonas.krahnde@gmail.com>
2025-01-17 21:21:45 +0100

    chore: Test

--- scripts/unix/generate-docs-packages.sh
+++ scripts/unix/generate-docs-packages.sh
@@ -4,8 +4,8 @@ echo "PACKAGE_NAME=$NAME" >> $GITHUB_ENV
 VERSION=$(node -p "require('./package.json').version")
 echo "PACKAGE_VERSION=$VERSION" >> $GITHUB_ENV
 
-mkdir -p "../../generated_docs/${PACKAGE_NAME}/${PACKAGE_VERSION}"
+mkdir -p "../../generated_docs/${NAME}/${VERSION}"
 
-echo "../../generated_docs/${PACKAGE_NAME}/${PACKAGE_VERSION}"
+echo "../../generated_docs/${NAME}/${VERSION}"
 
-cp -r docs/* "../../generated_docs/${PACKAGE_NAME}/${PACKAGE_VERSION}"
+cp -r docs/* "../../generated_docs/${NAME}/${VERSION}"
