ba065bf99053e37c450d470fac1e97c35078d43a
Jonas Krahn <joerg.jonas.krahn@iav.de>
2025-01-17 20:05:00 +0100

    chore: Test

--- scripts/unix/generate-docs-packages.sh
+++ scripts/unix/generate-docs-packages.sh
@@ -8,4 +8,6 @@ echo "PACKAGE_DOCS_FOLDER=$NAME _ $VERSION" >> $GITHUB_ENV
 
 mkdir -p generated_docs/$PACKAGE_DOCS_FOLDER
 
+echo "!!!!!!!!! generated_docs/$PACKAGE_DOCS_FOLDER"
+
 cp -r docs/* generated_docs/$PACKAGE_DOCS_FOLDER
