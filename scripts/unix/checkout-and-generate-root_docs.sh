165c2700aa249c8e6732de05693f3ed8f4ea2cac
Jonas Krahn <joerg.jonas.krahn@iav.de>
2025-01-17 17:28:17 +0100

    chore: Fix bug

--- scripts/unix/checkout-and-generate-root_docs.sh
+++ scripts/unix/checkout-and-generate-root_docs.sh
@@ -1,6 +1,3 @@
-git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-git config --global user.name "${GITHUB_ACTOR}"
-
 git fetch origin
 
 if git rev-parse --verify origin/gh-pages; then
