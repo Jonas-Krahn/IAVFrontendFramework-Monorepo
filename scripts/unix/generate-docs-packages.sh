0d09564f52c85a6ec50eb77b02e400d98cf962b9
Jonas Krahn <joerg.jonas.krahn@iav.de>
2025-01-17 20:09:52 +0100

    chore: Test

--- scripts/unix/generate-docs-packages.sh
+++ scripts/unix/generate-docs-packages.sh
@@ -4,7 +4,7 @@ echo "PACKAGE_NAME=$NAME" >> $GITHUB_ENV
 VERSION=$(node -p "require('./package.json').version")
 echo "PACKAGE_VERSION=$VERSION" >> $GITHUB_ENV
 
-echo "PACKAGE_DOCS_FOLDER=$NAME _ $VERSION" >> $GITHUB_ENV
+echo "PACKAGE_DOCS_FOLDER=${NAME}_${VERSION}" >> $GITHUB_ENV
 
 mkdir -p generated_docs/$PACKAGE_DOCS_FOLDER
 
