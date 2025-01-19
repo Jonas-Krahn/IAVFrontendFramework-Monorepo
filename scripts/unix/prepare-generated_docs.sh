1d43f32c6c26cb0e29d62ab2fb20a1f72f3207a1
Joerg Jonas Krahn <jonas.krahnde@gmail.com>
2025-01-19 18:01:02 +0100

    chore: Test

--- scripts/unix/prepare-generated_docs.sh
+++ scripts/unix/prepare-generated_docs.sh
@@ -1,6 +1,14 @@
 # This script should be executed inside the main folder
-if [! -d "generated_docs" ]; then
-    mkdir -p generated_docs
+
+DOCS_DIR="generated_docs"
+PACKAGES_DOCS_DIR="${DOCS_DIR}/packages"
+
+if [! -d $DOCS_DIR ]; then
+    mkdir -p $DOCS_DIR
 fi
 
-cp -r docs/* generated_docs
+if [! -d $PACKAGES_DOCS_DIR ]; then
+    mkdir -p $PACKAGES_DOCS_DIR
+fi
+
+cp -r docs/* $DOCS_DIR
