1c588dae38fae31ce90803a1e38f77822640f3f6
Jonas Krahn <joerg.jonas.krahn@iav.de>
2025-01-20 14:18:13 +0100

    chore: Test

--- scripts/unix/copy-root-docs-and-prepare.sh
+++ scripts/unix/copy-root-docs-and-prepare.sh
@@ -3,11 +3,11 @@
 DOCS_DIR="generated_docs"
 PACKAGES_DOCS_DIR="${DOCS_DIR}/packages"
 
-mkdir -p $DOCS_DIR
-
 mkdir -p $PACKAGES_DOCS_DIR
 
 # Copy the entry point docs to generated_docs
 cp -r docs/* $DOCS_DIR
 
-#cd $DOCS_DIR
+cd $DOCS_DIR
+
+pdw
