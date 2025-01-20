46fc7ec01123c2ce6757716c9596566496b4e3f6
Jonas Krahn <joerg.jonas.krahn@iav.de>
2025-01-20 18:50:47 +0100

    chore: Copy other relevant files

---
+++ scripts/unix/copy-additional-files.sh
@@ -0,0 +1,18 @@
+
+#!/bin/sh
+# IMPORTANT: This script should be executed in the root folder of the repository.
+
+# Define an array of files to copy
+FILES=(".gitignore", "CHANGELOG.md", "CONTRIBUTING.md", "LICENSE", "NOTICE.md", "README.md", "SBOM.spdx")
+
+# Define source and target directories
+SRC_DIR="main"
+TARGET_DIR="."
+
+for file in "${FILES[@]}"
+do
+    # For generelization: Ensure target directory structure exists
+    mkdir -p "$(dirname "$TARGET_DIR/$file")"
+
+    cp "$SRC_DIR/$file" "$TARGET_DIR/$file"
+done
