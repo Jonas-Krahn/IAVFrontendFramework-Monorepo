14562e96cf5f1ab55b940f5d0f11df2775d4d75d
Jonas Krahn <joerg.jonas.krahn@iav.de>
2025-01-20 19:03:21 +0100

    chore: Fix Bug

--- scripts/unix/copy-additional-files.sh
+++ scripts/unix/copy-additional-files.sh
@@ -3,16 +3,17 @@
 # IMPORTANT: This script should be executed in the root folder of the repository.
 
 # Define an array of files to copy
-FILES=(".gitignore", "CHANGELOG.md", "CONTRIBUTING.md", "LICENSE", "NOTICE.md", "README.md", "SBOM.spdx")
+FILES=".gitignore CHANGELOG.md CONTRIBUTING.md LICENSE NOTICE.md README.md SBOM.spdx"
 
 # Define source and target directories
-SRC_DIR="main"
+SRC_DIR="./main"
 TARGET_DIR="."
 
-for file in "${FILES[@]}"
+for file in $FILES
 do
     # For generelization: Ensure target directory structure exists
     mkdir -p "$(dirname "$TARGET_DIR/$file")"
 
+    # Copy file from source to target
     cp "$SRC_DIR/$file" "$TARGET_DIR/$file"
 done
