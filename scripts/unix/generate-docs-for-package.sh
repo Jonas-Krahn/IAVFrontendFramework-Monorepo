7e16849a34b519b9b3565c73f2ad37727a7843de
Jonas Krahn <joerg.jonas.krahn@iav.de>
2025-01-20 17:37:30 +0100

    chore: Update

--- scripts/unix/generate-docs-for-package.sh
+++ scripts/unix/generate-docs-for-package.sh
@@ -2,9 +2,6 @@
 
 DOCS_DIR="../../generated_docs/packages"
 
-PACKAGE_DOCS_DIR="docs-package"
-VERSION_DOCS_DIR="docs-version"
-
 NAME=$(node -p "require('./package.json').name")
 VERSION=$(node -p "require('./package.json').version")
 
@@ -14,8 +11,8 @@ VERSION_DOCS_PATH="${DOCS_DIR}/${NAME}/${VERSION}"
 mkdir -p $PACKAGE_DOCS_PATH
 mkdir -p $VERSION_DOCS_PATH
 
-cp -r "${PACKAGE_DOCS_DIR}/*" $PACKAGE_DOCS_PATH
+cp -r docs-package/* $PACKAGE_DOCS_PATH
 
-cp -r "${VERSION_DOCS_DIR}/*" $VERSION_DOCS_PATH
+cp -r docs-version/* $VERSION_DOCS_PATH
 
 echo "Created docs for ${NAME}"
