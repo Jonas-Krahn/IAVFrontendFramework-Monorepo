4b2c64c18c54962dbb8cb886ba1cf31b0bcca351
Jonas Krahn <joerg.jonas.krahn@iav.de>
2025-01-20 18:35:47 +0100

    chore: Introduced new variables

--- scripts/unix/generate-docs-for-package.sh
+++ scripts/unix/generate-docs-for-package.sh
@@ -1,6 +1,8 @@
 # IMPORTANT: This script should be executed in the context of packages.
 
 DOCS_DIR="../../generated_docs/packages"
+DOCS_PACKAGE_DIR="docs-package"
+DOCS_VERSION_DIR="docs-version"
 
 NAME=$(node -p "require('./package.json').name")
 VERSION=$(node -p "require('./package.json').version")
@@ -11,8 +13,8 @@ VERSION_DOCS_PATH="${DOCS_DIR}/${NAME}/${VERSION}"
 mkdir -p $PACKAGE_DOCS_PATH
 mkdir -p $VERSION_DOCS_PATH
 
-cp -r docs-package/* $PACKAGE_DOCS_PATH
+cp -r $DOCS_PACKAGE_DIR/* $PACKAGE_DOCS_PATH
 
-cp -r docs-version/* $VERSION_DOCS_PATH
+cp -r $DOCS_VERSION_DIR/* $VERSION_DOCS_PATH
 
 echo "Created docs for ${NAME}"
